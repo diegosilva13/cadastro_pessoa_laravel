32dd7290059bdf382c566d8aa52621dfa3f4360a
Diego Brener da Silva <diego.dbssilva@gmail.com>
2017-03-01 21:39:19 -0400

    mudando nome dos diretórios para portugues

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
 Route::get('/', function () {
-		return view('person.index');
+		return view('perssoa.index');
 	});
 
 Route::get('/create', function () {
-		return view('person.create');
+		return view('perssoa.create');
 	});
 
 Route::get('/show', function () {
-		return view('person.show');
+		return view('perssoa.show');
 	});
 
 Route::get('/edit', function () {
-		return view('person.edit');
+		return view('perssoa.edit');
 	});
